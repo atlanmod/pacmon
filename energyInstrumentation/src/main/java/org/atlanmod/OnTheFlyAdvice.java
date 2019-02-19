5eea7d29ad9203051106eb0d08286b983cf3a3d5
Alexis Pineau <alexis@paradise-city.lan>
2019-02-19 17:37:21 +0100

    Modifs pour récupérer les puissances

--- energyInstrumentation/src/main/java/org/atlanmod/OnTheFlyAdvice.java
+++ energyInstrumentation/src/main/java/org/atlanmod/OnTheFlyAdvice.java
@@ -1,22 +1,32 @@
 package org.atlanmod;
 
 import net.bytebuddy.asm.Advice;
+import org.powerapi.PowerDisplay;
+import org.powerapi.core.power.Power;
+import org.powerapi.core.target.Target;
+import scala.collection.immutable.Set;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class OnTheFlyAdvice {
 
     @Advice.OnMethodEnter
     static void enter(@Advice.Local("monitor") Monitor monitor) {
-
+        PowerDisplay display = new PowerDisplay() {
+            @Override
+            public void display(UUID muid, long timestamp, Set<Target> targets, Set<String> devices, Power power) {
+                System.out.print(timestamp + " : ");
+                System.out.println(power.toMilliWatts());
+            }
+        };
         monitor = new MonitorBuilder()
                 .withDuration(60, TimeUnit.SECONDS)
                 .withRefreshFrequency(100, TimeUnit.MILLISECONDS)
                 .withTdp(15)
                 .withTdpFactor(0.7)
-                .withChartDisplay()
+                .withCustomDisplay(display)
                 .build();
-
         monitor.run((int) SystemUtils.getPID());
     }
 
