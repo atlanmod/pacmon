452bf75c0ffd7b1400c5e98f553328ff611fc28a
Raphael <page.raf@gmail.com>
2019-03-01 13:12:55 +0100

    recuperation du pid d'un programme exterieur au projet

--- energyInstrumentation/src/main/java/org/atlanmod/App.java
+++ energyInstrumentation/src/main/java/org/atlanmod/App.java
@@ -1,14 +1,38 @@
 package org.atlanmod;
 
+import org.apache.commons.lang.UnhandledException;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Field;
+
 /**
  * Hello world!
  *
  */
 public class App
 {
-    public static void main( String[] args ) throws InterruptedException
+    public static void main( String[] args ) throws Exception
     {
 
+
+        Process process = Runtime.getRuntime().exec("java -jar src/main/resources/processBenchmark-1.0-SNAPSHOT.jar");
+
+        //get the input stream of the process to displayit's pid and check we've got the good one
+        InputStream input = process.getInputStream();
+        System.out.println(input.read());
+
+        //get the pid field of the process
+        Field f = process.getClass().getDeclaredField("pid");
+        f.setAccessible(true);
+        int pid = (int) f.get(process);
+
+        System.out.println(pid);
+
+        process.destroy();
+
+
+        /*
 
         Thread.sleep(10000);
 
@@ -18,7 +42,7 @@ public class App
             k++;
         }
 
-        Thread.sleep(5000);
+        Thread.sleep(5000);*/
 
 
     }
