0239b855e7f9ee36e0c4b89f91af20b96360a6c6
Louis <1questel.louis@gmail.com>
2019-03-22 11:47:27 +0100

    ajout de la classe GlobalBufferWriter pour optimiser l'action d'écriture dans un fichier

--- energyInstrumentation/src/main/java/org/atlanmod/GlobalBufferWriter.java
+++ energyInstrumentation/src/main/java/org/atlanmod/GlobalBufferWriter.java
@@ -4,11 +4,13 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 
-public class GlobalBufferWriter {
+public final class GlobalBufferWriter {
+    private static volatile GlobalBufferWriter globalBufferWriter = null;
     public static FileWriter fw;
     public static BufferedWriter bw;
 
-    GlobalBufferWriter(String pathFile) {
+    private GlobalBufferWriter(String pathFile) {
+        super();
         try {
             File file = new File(pathFile);
             if (!file.exists()) {
@@ -23,31 +25,40 @@ public class GlobalBufferWriter {
             System.out.println("Fail to create the FileWriter for the file : " + pathFile);
         }
     }
-    /*
-    writeInFile(float mesure){}
-        finally{
-            try {
-                if (bw != null) {
-                    bw.close();
-                }
-                if (fw != null) {
-                    fw.close();
-                }
-            }
-            catch(Exception e){
-                System.out.println("Fail to close the FileWriter for the file : " + pathFile);
 
+    public final static GlobalBufferWriter getInstance(String pathFile) {
+        //Le "Double-Checked Singleton"/"Singleton doublement vérifié" permet
+        //d'éviter un appel coûteux à synchronized,
+        //une fois que l'instanciation est faite.
+        if (GlobalBufferWriter.globalBufferWriter == null) {
+            // Le mot-clé synchronized sur ce bloc empêche toute instanciation
+            // multiple même par différents "threads".
+            // Il est TRES important.
+            synchronized(GlobalBufferWriter.class) {
+                if (GlobalBufferWriter.globalBufferWriter == null) {
+                    GlobalBufferWriter.globalBufferWriter = new GlobalBufferWriter(pathFile);
+                }
             }
         }
+        return GlobalBufferWriter.globalBufferWriter;
     }
-    */
-    public static void main( String[] args){
-        System.out.println("test de la science universelle");
-        new GlobalBufferWriter("/home/louis/git/TER_1_03_2019/jPowerApi/energyInstrumentation/src/main/resources/outputTER333.txt");
+
+    public void writeInfile(float s){
         try {
-            GlobalBufferWriter.bw.write("lol56lolol");
-            GlobalBufferWriter.bw.close();
-            GlobalBufferWriter.fw.close();
+            GlobalBufferWriter.getInstance("").bw.write(s + "\n");
+        }
+        catch(Exception e){
+
+        }
+    }
+
+    public static void main( String[] args) {
+        System.out.println("test de la science universelle");
+        GlobalBufferWriter gb = GlobalBufferWriter.getInstance("/home/louis/git/TER_1_03_2019/jPowerApi/energyInstrumentation/src/main/resources/outputTER333.txt");
+        try {
+            gb.bw.write("lol56lolol");
+            gb.bw.close();
+            gb.fw.close();
         }
         catch (Exception e) {
             System.out.println("lol ça beug");
