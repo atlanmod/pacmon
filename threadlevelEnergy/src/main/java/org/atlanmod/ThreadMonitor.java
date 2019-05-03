f22c5d0a7c6748b690685a67a4b22ffccd58b049
Thibault Béziers la Fosse <thiblf@gmail.com>
2019-05-03 14:11:42 +0200

    Added a Thread monitor computing CPU usage of given thread with given Thread ID. The value hence gathered could be used as a basis for computing power consumption using PowerAPIs formula.

---
+++ threadlevelEnergy/src/main/java/org/atlanmod/ThreadMonitor.java
@@ -0,0 +1,69 @@
+package org.atlanmod;
+
+import com.jvmtop.monitor.VMInfo;
+import com.jvmtop.openjdk.tools.LocalVirtualMachine;
+import com.jvmtop.openjdk.tools.ProxyClient;
+
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+public class ThreadMonitor {
+
+    private int tid;
+
+    private double oldCPUUptime;
+    private double oldThreadCPUTime;
+    private double CPUUptime;
+    private double threadCPUTime;
+    private double threadDelta;
+    private double delta;
+
+    private ThreadMXBean threadMXBean;
+    private ProxyClient proxyClient;
+    private VMInfo vmInfo;
+
+    public ThreadMonitor(int VMPID, int tid) {
+        try {
+            this.tid = tid;
+            vmInfo = VMInfo.processNewVM(LocalVirtualMachine.getLocalVirtualMachine(VMPID),1);
+            proxyClient = vmInfo.getProxyClient();
+            threadMXBean = vmInfo.getThreadMXBean();
+            oldCPUUptime = proxyClient.getProcessCpuTime();
+            threadCPUTime = threadMXBean.getThreadCpuTime(tid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public double getThreadCPUUsage() {
+        try {
+            update();
+            return (threadDelta  / delta);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    /**
+     * Updates system values
+     * @throws Exception if {@link ProxyClient} cannot fetch CPU times
+     */
+    private void update() throws Exception {
+        proxyClient.flush();
+
+        //Getting new thread values
+        oldThreadCPUTime = threadCPUTime;
+        threadCPUTime = threadMXBean.getThreadCpuTime(tid);
+
+        //Getting new CPU values
+        oldCPUUptime = CPUUptime;
+        CPUUptime = proxyClient.getProcessCpuTime();
+
+        //Computing Delta since last update to get average running duration
+        threadDelta = threadCPUTime - oldThreadCPUTime;
+        delta = CPUUptime - oldCPUUptime;
+    }
+
+}
+
