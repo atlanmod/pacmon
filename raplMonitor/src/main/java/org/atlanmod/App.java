d830841e4d99834f6c239ac9a6ef9c4cd309894e
alexis <alexis.loret56@gmail.com>
2019-05-06 11:47:57 +0200

    100 tests are now done for raplMonitor and the average is calculated and written in a file

--- raplMonitor/src/main/java/org/atlanmod/App.java
+++ raplMonitor/src/main/java/org/atlanmod/App.java
@@ -1,5 +1,11 @@
 package org.atlanmod;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
 
 /**
@@ -10,19 +16,51 @@ public class App
 {
     public static void main( String[] args )
     {
-        double timestart = System.nanoTime();
-        double start = RAPLMonitor.getEnergy();
-        double number = Math.pow(10, 10); // To change the number of Throws
-        double res = computePI(number);
-        double end = RAPLMonitor.getEnergy();
-        double timeend = System.nanoTime();
-        System.out.println("Résultat de PI : "+res);
-        double energy = end-start;
-        double time = timeend-timestart;
-        System.out.println("Energie consommée (J): "+energy/1000000);
-        System.out.println("Temps : "+time);
-        double puissance = (energy*1000) / time;
-        System.out.println("Puissance : "+puissance);
+        String sPathRes = "./raplMonitor/src/main/resources/resultatTER.txt";
+        try {
+
+            Files.delete(Paths.get(sPathRes));
+
+
+            Files.createFile(Paths.get(sPathRes));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        GlobalBufferWriter.getInstance("./raplMonitor/src/main/resources/resultatTER.txt");
+        double total = 0;
+        for(int i = 0; i < 100; i++) {
+            double timestart = System.nanoTime();
+            double start = RAPLMonitor.getEnergy();
+            double number = Math.pow(10, 7); // To change the number of Throws
+            double res = computePI(number);
+            double end = RAPLMonitor.getEnergy();
+            double timeend = System.nanoTime();
+          //  System.out.println("Résultat de PI : "+res);
+            double energy = end-start;
+            double time = timeend-timestart;
+           // System.out.println("Energie consommée (J): "+energy/1000000);
+           // System.out.println("Temps : "+time);
+            double puissance = (energy*1000) / time;
+           // System.out.println("Puissance : "+puissance);
+            total +=(energy/1000000);
+        }
+        try {
+            GlobalBufferWriter.bw.write("Moyenne de l'energie en watt : "+total/100+"  ");
+            GlobalBufferWriter.bw.close();
+            GlobalBufferWriter.fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ;
+
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("./raplMonitor/src/main/resources/resultatTER.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
     // Calculates PI based on the number of throws versus misses
