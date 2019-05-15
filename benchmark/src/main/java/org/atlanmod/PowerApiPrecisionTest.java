57e5cdfc4476c2b78367445996449171be5abff4
Louis <1questel.louis@gmail.com>
2019-05-15 16:39:00 +0200

    another update for PwerAPIPrecisionTest

--- benchmark/src/main/java/org/atlanmod/PowerApiPrecisionTest.java
+++ benchmark/src/main/java/org/atlanmod/PowerApiPrecisionTest.java
@@ -5,20 +5,33 @@ import org.atlanmod.metrics.PowerApiMonteCarlo;
 import java.util.concurrent.TimeUnit;
 
 public class PowerApiPrecisionTest {
-    static int freqMaxForTest = 400;
-    static int stepfreq = 10;
+    static int[] fregtToTest;
     static int nbOccurenceForTest = 20;
 
     public static void main(String args[]){
+        // on remplit le tableau freqToTest
+        int otherCpt = 0;
+        for (int i=1;i<=512;i=i*2){
+            otherCpt++;
+        }
+        fregtToTest = new int[otherCpt];
+        otherCpt = 0;
+        for (int i=1;i<=512;i=i*2){
+            fregtToTest[otherCpt] = i;
+            otherCpt++;
+        }
+        System.out.println(otherCpt);
+
         // création du moniteur de test
         double[] results = new double[nbOccurenceForTest];
-        double[][] finalRes = new double[(int)(freqMaxForTest/stepfreq)+1][2];
+        double[][] finalRes = new double[fregtToTest.length][2];
+        int cptForFinalRes = 0;
 
-        for (int frequence=freqMaxForTest; frequence >= 0; frequence -= stepfreq) {
+        for(int freq : fregtToTest) {
             // test pour 20 itérations
             for (int i = 0; i < nbOccurenceForTest; i++) {
-                PowerApiMonteCarlo pm = new PowerApiMonteCarlo(7, false);
-                pm.setMonitorFrequency(frequence, TimeUnit.MILLISECONDS);
+                PowerApiMonteCarlo pm = new PowerApiMonteCarlo(8, false);
+                pm.setMonitorFrequency(freq, TimeUnit.MILLISECONDS);
                 String filePath = pm.run();
                 results[i] = new EnergyCalculator().run(filePath);
             }
@@ -34,14 +47,25 @@ public class PowerApiPrecisionTest {
             }
             variance = variance / results.length;
             double ecarttype = Math.sqrt(variance);
-            finalRes[frequence/stepfreq][0] = frequence;
-            finalRes[frequence/stepfreq][1] = ecarttype/1000;
+
+
+            finalRes[cptForFinalRes][0] = freq;
+            finalRes[cptForFinalRes][1] = ecarttype/1000;
+            cptForFinalRes++;
         }
+        // variable pour latex
+        String graph = "{";
         for (int i=0; i<finalRes.length;i++){
             System.out.println("----------------------------------------------------------");
             System.out.println("Ecart-type pour frequence,"+finalRes[i][0]+" : "+finalRes[i][1]);
+            graph = graph + "("+finalRes[i][0]+","+finalRes[i][1]+") \n";
         }
+        graph = graph + "};";
+
         System.out.println("Results.size : "+results.length);
         System.out.println("FinalRes.size : "+finalRes.length);
+        System.out.println("----------------------------------------------------------");
+        System.out.println(graph);
+
     }
 }
