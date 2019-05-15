9950275aa4686b0e67d76892475e529bed1b427c
Louis <1questel.louis@gmail.com>
2019-05-15 11:48:11 +0200

    updating class PowerApiPrecisionTest

--- benchmark/src/main/java/org/atlanmod/PowerApiPrecisionTest.java
+++ benchmark/src/main/java/org/atlanmod/PowerApiPrecisionTest.java
@@ -1,20 +1,22 @@
-package org.atlanmod.metrics;
+package org.atlanmod;
 import org.atlanmod.*;
+import org.atlanmod.metrics.PowerApiMonteCarlo;
 
 import java.util.concurrent.TimeUnit;
 
 public class PowerApiPrecisionTest {
-    static int freqMaxForTest = 200;
+    static int freqMaxForTest = 400;
     static int stepfreq = 10;
+    static int nbOccurenceForTest = 20;
 
     public static void main(String args[]){
         // création du moniteur de test
-        double[] results = new double[freqMaxForTest/10];
-        double[][] finalRes = new double[freqMaxForTest/5][2];
+        double[] results = new double[nbOccurenceForTest];
+        double[][] finalRes = new double[(int)(freqMaxForTest/stepfreq)+1][2];
 
         for (int frequence=freqMaxForTest; frequence >= 0; frequence -= stepfreq) {
             // test pour 20 itérations
-            for (int i = 0; i < 20; i++) {
+            for (int i = 0; i < nbOccurenceForTest; i++) {
                 PowerApiMonteCarlo pm = new PowerApiMonteCarlo(7, false);
                 pm.setMonitorFrequency(frequence, TimeUnit.MILLISECONDS);
                 String filePath = pm.run();
@@ -35,9 +37,11 @@ public class PowerApiPrecisionTest {
             finalRes[frequence/stepfreq][0] = frequence;
             finalRes[frequence/stepfreq][1] = ecarttype/1000;
         }
-        for (int i=0; i<=freqMaxForTest/stepfreq;i++){
+        for (int i=0; i<finalRes.length;i++){
             System.out.println("----------------------------------------------------------");
             System.out.println("Ecart-type pour frequence,"+finalRes[i][0]+" : "+finalRes[i][1]);
         }
+        System.out.println("Results.size : "+results.length);
+        System.out.println("FinalRes.size : "+finalRes.length);
     }
 }
